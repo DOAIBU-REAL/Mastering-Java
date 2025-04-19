a2bd6a594b367dedfdd3ac1d8d1edd59342af5e5
Muhammad Arifin <akhi.arifin19@gmail.com>
2025-04-19 23:12:24 +0700

    Add files via upload

---
+++ Java 07 Memahami String[]args/Java_07_Memahami_String_args/src/main/java/com/tutorial/Main.java
@@ -0,0 +1,20 @@
+
+package com.tutorial;
+
+
+public class Main {
+    
+    public static void main(String[]args){
+        System.out.println(args[0]); 
+        System.out.println(args[1]);
+        
+        System.out.println("hello stalkers");
+        System.out.println("dapat disimpulkan perintah string dalam tanda "
+                + "kotak kurung args merupakan sebuah data yang mengandung indeks"
+                + " angka yang mewakili setiap data string atau kata"
+                + " saat kita ketik lewat"
+                + "printah println");
+        
+    }
+    
+}
