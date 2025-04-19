3c5d147bc4f3fe4899000c0bb35895b638d505fe
Muhammad Arifin <akhi.arifin19@gmail.com>
2025-04-19 22:26:23 +0700

    Add files via upload

---
+++ Java 06 Pengenalan Print dan Alur Eksekusi/Java_06_Pengenalan_Print_dan_Alur_Eksekusi/src/main/java/com/tutorial/Main.java
@@ -0,0 +1,20 @@
+
+package com.tutorial;
+
+public class Main {
+    
+    public static void main(String[]args){
+        
+        System.out.println("hello world");
+        System.out.println("hello too");
+        System.out.println("Perintah println atau printline memberikan efek 'enter ' setiap barisan string, ");
+        
+        System.out.print("perintah print 1 :sedangkan perintah print saja tidak memberikan efek 'enter', ");
+        System.out.print("perintah print 2 :dapat dilihat hasilnya apabila tidak menggunakan perintah println akan menyatu ");
+        
+        System.out.println("kemudian ada perintah ' printf ' atau print format yang mampu memberikan cetakan kombinasi ");
+        System.out.printf("contoh hasil perintah seperti ini %d", 212  );
+        System.out.printf("   .maka dapat dismpulkakn jenis perintah mencetak berjumlah %d", 3, "macam");
+        
+    }
+}
