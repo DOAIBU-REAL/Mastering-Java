e38a06b15a0db1a5c1378fb0efc37d83955cf769
Muhammad Arifin <akhi.arifin19@gmail.com>
2025-04-20 23:29:56 +0700

    Add files via upload

---
+++ Java 08 Variable, Assignment dan Deklarasi/08_Variable_Assignment_dan_Deklarasi/src/main/java/com/tutorial/Main.java
@@ -0,0 +1,25 @@
+
+package com.tutorial;
+
+
+public class Main {
+    public static void main (String[]args){
+        //ini adalah komen
+        // kita akan membuat variable
+        int x = 10; // perintah ini bisa disebut assignment
+        System.out.println("nilai x = " + x);
+        
+        x = 20;
+        System.out.println("nilai x baru =" + x );
+        
+        // kita akan membuat deklarasi
+        int b; // perintah ini bisa disebut deklarasi
+        
+        b = 7;
+        System.out.println("nilai deklarasi b =" + b );
+        
+        
+        
+    }
+    
+}
